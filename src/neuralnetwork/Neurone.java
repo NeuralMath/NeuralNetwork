5cba97b3f924603ce1df9bdc04f4a1727b26cb10
Marc-Antoine <marc4492@gmail.com>
2017-02-10 22:49:05 -0500

    Reseau à 9 neurones avec les liaisons fonctionnelles.

--- src/neuralnetwork/Neurone.java
+++ src/neuralnetwork/Neurone.java
@@ -1,26 +1,66 @@
 package neuralnetwork;
 
-public abstract class Neurone {
+/**
+ * Class qui contient l'exécution d'une neurone selon les input(avec les weights associé) 
+ * 
+ * @author Marc4492
+ * 10 février 2017
+ */
+public class Neurone {
     
-    protected double inputValue = 0;
-    protected double output;
-    protected int position;
-        
+    private double inputValue = 0;
+    private double output;
+    private final int position;
+    private final double[] weights;
+    
+    /**
+     * Constructeur pour les neurones sans weight (input layer)
+     * 
+     * @param pos Position de la neurone dans le layer quel appartient
+     */
     public Neurone(int pos) {
         position = pos;
+        weights = null;
+    }
+    
+    /**
+     * Constructeur pour les neurones avec weight (input layer)
+     * 
+     * @param pos       Position de la neurone dans le layer quel appartient
+     * @param weight    Tableau des weights de toutes les liaisons en amont de la neurone
+     */
+    public Neurone(int pos, double[] weight) {
+        position = pos;
+        weights = weight;
     }
 
+    /**
+     * Obtenir l'output de la neurone
+     * 
+     * @return  L'output
+     */
     public double getOutput() {
         return output;
     }
 
-    public abstract void addInputs(double input, int index);
-    
-    public abstract void computes();
-    
-    public void sendValue(Neurone[] goalNeuronne)
+    /**
+     * Ajouté un input à la neurone
+     * 
+     * @param input     La valeur de l'input
+     * @param index     L'index de la neurone qui envoie ce input pour trouver le weight
+     */
+    public void addInputs(double input, int index)
     {
-        for (Neurone n : goalNeuronne)
-            n.addInputs(output, position);
+        if(weights == null)
+            inputValue += input;
+        else
+            inputValue += weights[index] * input;
+    }
+    
+    /**
+     * Calculer la valeur de l'output
+     */
+    public void computes() {
+        output = 1 / (1 + Math.exp(inputValue));
     }
 }
--- src/neuralnetwork/Reseau.java
+++ src/neuralnetwork/Reseau.java
@@ -1,76 +1,114 @@
 package neuralnetwork;
 
+/**
+ * Class qui contient le réseau de neurones et qui peut obtenir la valeur de sortie du réseau
+ * 
+ * @author Marc4492
+ * 10 février 2017
+ */
 public class Reseau {
     
-    private final InputNeurone[] inputLayer = new InputNeurone[4];
-    private final GeneralNeurone[] hiddenLayer = new GeneralNeurone[3];
-    private final GeneralNeurone[] outputLayer = new GeneralNeurone[2];
-    
+    private final double[][][] weights;
     private final Neurone[][] reseau = 
     {
-        inputLayer,
-        hiddenLayer,
-        outputLayer
+        new Neurone[4],
+        new Neurone[3],
+        new Neurone[2]
     };
-    
-    private final double[][] weights;
 
-    public Reseau(double[][] weightsTemp) throws IllegalArgumentException{
+    /**
+     * Constructeur du réseau, création des neurones
+     * 
+     * @param weightsTemp                   Le tableau des weight des liaison entre les neurones
+     * @throws IllegalArgumentException     Si le tableau de weight n'a pas les bonne grandeur
+     */
+    public Reseau(double[][][] weightsTemp) throws IllegalArgumentException{
+        
+        //Vérification pour la grandeur en toutes les dimensions du tableau de wieght
         if(weightsTemp.length != reseau.length-1)
-            throw new IllegalArgumentException("Le tableau de weight a pas la bonne grandeur");
+            throw new IllegalArgumentException("Le tableau de weight a pas la bonne grandeur par rapport au nombre de layer");
         else
-            for(int i = 0; i < reseau.length-1; i++)
-                if(weightsTemp[i].length != reseau[i].length * reseau[i+1].length)
-                    throw new IllegalArgumentException("Le tableau de weight a pas la bonne grandeur" + i);
+        {
+            for(int i = 1; i < reseau.length; i++)
+            {
+                if(weightsTemp[i-1].length != reseau[i].length)
+                    throw new IllegalArgumentException("Le tableau de weight a pas la bonne grandeur par rapport au nombre de neurones dans le layer " + i);
+                
+                for(int j = 0; j < reseau[i].length; j++)
+                    if(weightsTemp[i-1][j].length != reseau[i-1].length)
+                        throw new IllegalArgumentException("Le tableau de weight a pas la bonne grandeur par rapport au nombre de liaisons de la neurone " + j + " dans le entre le layer " + (i-1) + " et " + i);
+            }
+        }
                     
         weights = weightsTemp;
         
+        //Création des neuronnes
         for(int i = 0; i < reseau.length; i++)
         {
             for(int j = 0; j < reseau[i].length; j++)
             {
+                //Creation des neurones d'entrée donc sans weight
                 if(i == 0)
-                    reseau[i][j] = new InputNeurone(reseau[1].length, j);
+                    reseau[i][j] = new Neurone(j);
                 else
-                    reseau[i][j] = new GeneralNeurone(j, weights[i-1]);
+                    reseau[i][j] = new Neurone(j, weights[i-1][j]);
             }
         }
     }
     
+    /**
+     * Pour entrer les entrées des neurones du premier layer
+     * 
+     * @param input                         Le tableau de double pour les entrées
+     * @throws IllegalArgumentException     S'il n'y a pas le bon nombre de données
+     */
     public void setInputs(double[] input) throws IllegalArgumentException
     {
-        if(input.length != inputLayer.length)
+        if(input.length != reseau[0].length)
             throw new IllegalArgumentException("Pas la bonne longueur de data");
         
         for(int i = 0 ; i < input.length; i++)
-            inputLayer[i].setInputs(input[i]);
+            reseau[0][i].addInputs(input[i], 0);
     }
     
+    /**
+     * Exectuer le réseau de neurones
+     * 
+     * @return Retourne la position de la neurone avec le plus grande valeur de sorite
+     */
     public int computes()
     {
-        int posHighest = -1;
-                
+        int posMax = 0;
+        
+        //Exécuter les neurones du premier étage et passer les valeurs à l'autre couche
         for(int i = 0; i < reseau[0].length; i++)
         {
             reseau[0][i].computes();
             for(int j = 0; j < reseau[1].length; j++)
             {
                 reseau[1][j].addInputs(reseau[0][i].getOutput(), i);
-                reseau[1][j].computes();
-                for(int k = 0; k < reseau[2].length; k++)
-                {
-                    reseau[2][k].addInputs(reseau[1][j].getOutput(), k);
-                    reseau[2][k].computes();
-                }
             }
         }
         
-        for(int i = 0; i < reseau[2].length; i++)
+        //Exécuter les neurones du deuxième étage et passer les valeurs à l'autre couche
+        for(int i = 0; i < reseau[1].length; i++)
         {
-            if(reseau[2][i].getOutput() > posHighest)
-                posHighest = i;
+            reseau[1][i].computes();
+            for(int j = 0; j < reseau[2].length; j++)
+            {
+                reseau[2][j].addInputs(reseau[1][i].getOutput(), i);
+            }
         }
         
-        return posHighest;
+        //Exécuter les neurones du dernier étage
+        for(int i = 0; i < reseau[2].length; i++)
+            reseau[2][i].computes();
+        
+        //Trouver la position de la plus grande valeur d'output
+        for(int i = 0; i < reseau[2].length; i++)
+            if(reseau[2][i].getOutput() > reseau[2][posMax].getOutput())
+                posMax = i;
+        
+        return posMax;
     }
 }
